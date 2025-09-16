e9b2763eaa99fcfe38d4550b35e06ed7763f0efa
gPagio <guilhermepagio.contato@gmail.com>
2025-09-16 17:19:55 -0300

    feat: secao 4 16/09/2025 17:19

---
+++ expenses/lib/components/transaction_form.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(labelText: 'Título'),
+            ),
+            TextField(
+              controller: valueController,
+              decoration: InputDecoration(labelText: 'Valor R\$'),
+            ),
+            TextButton(
+              onPressed: () {
+                print("Titulo ${titleController.text}");
+                print("Valor ${valueController.text}");
+              },
+              child: Text(
+                'Nova Transação',
+                style: TextStyle(color: Colors.purple),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ expenses/lib/components/transaction_list.dart
@@ -0,0 +1,55 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> _transactions;
+
+  const TransactionList(this._transactions, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      /* * Children recebe listas e estamos
+         * retornando uma lista de Cards
+         * para esta propriedade de Column
+         * */
+      children: _transactions.map((t) {
+        return Card(
+          child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple, width: 2),
+                ),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${t.value.toStringAsFixed(2).replaceAll('.', ',')}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    t.title,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    DateFormat('d MMM y').format(t.date),
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
