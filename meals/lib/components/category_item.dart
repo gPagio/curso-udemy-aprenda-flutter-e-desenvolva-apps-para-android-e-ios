91a6fec0474873d49ad3f5ceffdb3790b7504168
gPagio <guilhermepagio.contato@gmail.com>
2025-10-17 19:42:39 -0300

    feat: secao 7 17/10/2025 19:42

--- meals/lib/components/category_item.dart
+++ meals/lib/components/category_item.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart' show Category;
+import 'package:meals/screens/categories_meals_screen.dart'
+    show CategoriesMealsScreen;
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem(this.category, {super.key});
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (_) => const CategoriesMealsScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [category.color.withValues(alpha: 0.5), category.color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [category.color.withValues(alpha: 0.5), category.color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
+        child: Text(category.title),
       ),
-      child: Text(category.title),
     );
   }
 }
--- meals/lib/screens/categories_meals_screen.dart
+++ meals/lib/screens/categories_meals_screen.dart
@@ -5,6 +5,9 @@ class CategoriesMealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Receitas')),
+      body: Center(child: Text('Receitas por Categoria')),
+    );
   }
 }
