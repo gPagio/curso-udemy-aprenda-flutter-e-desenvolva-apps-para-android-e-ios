f999fcfe1b8ee5ae838ecf7b2db38d0aa766724e
gPagio <guilhermepagio.contato@gmail.com>
2026-02-04 14:54:22 -0300

    fix: badge nao clicavel carrinho
    Ao tentar clicar no icone do carrinho, se clicado em cima da badge que mostra a quantidade de itens do mesmo, o clique nao era computado, pois a badge impedia o clique no IconButton que esta por tras dela

--- shop/lib/components/badge.dart
+++ shop/lib/components/badge.dart
@@ -21,17 +21,19 @@ class Badge extends StatelessWidget {
         Positioned(
           right: 8,
           top: 8,
-          child: Container(
-            padding: const EdgeInsets.all(2),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: color ?? Theme.of(context).colorScheme.secondary,
-            ),
-            constraints: BoxConstraints(minHeight: 16, minWidth: 16),
-            child: Text(
-              value,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 10),
+          child: IgnorePointer(
+            child: Container(
+              padding: const EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: color ?? Theme.of(context).colorScheme.secondary,
+              ),
+              constraints: BoxConstraints(minHeight: 16, minWidth: 16),
+              child: Text(
+                value,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 10),
+              ),
             ),
           ),
         ),
